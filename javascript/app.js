a06093e416f6f79df014bfbc39c7e0920cd64b7c
Phi Hai Nguyen <phihai93@gmail.com>
2019-10-04 16:13:29 -0700

    updaate

--- javascript/app.js
+++ javascript/app.js
@@ -4,9 +4,12 @@ let guessContainer = document.querySelector(".guessContainer");
 let guessInput = document.querySelector("#guessInput");
 let images = document.querySelectorAll("img");
 let userGuess = document.querySelector(".userGuess");
+let newEl = document.createElement("span");
+let el;
 let inputVal;
 let rope = "./ropeIcon.png";
 let span;
+let letter;
 
 wordInput.addEventListener("keyup", getWord);
 guessInput.addEventListener("keyup", checkGuess);
@@ -42,16 +45,22 @@ function checkGuess(event) {
   console.log(guessInput.value);
   console.log(guessWord);
   inputVal = guessInput.value;
-  if (guessWord.includes(inputVal)) {
-    console.log("found match");
-  } else {
-    console.log("no match found");
-  }
-  //   for (let i = 0; i < guessWord.length; i++) {
-  //     if (inputVal === guessWord[i]) {
-  //       console.log("(its a match " + i);
-  //     } else {
-  //       console.log("not a match");
+  //   if (guessWord.includes(inputVal)) {
+  //     let place = guessWord.indexOf(inputVal);
+  //     console.log(place);
+  //     if (guessWord.indexOf(inputVal) !== -1) {
+  //       console.log(guessWord.indexOf(inputVal, place + 1));
   //     }
+  //   } else {
+  //     console.log("no match found");
   //   }
+  for (let i = 0; i < guessWord.length; i++) {
+    if (inputVal === guessWord[i]) {
+      document
+        .getElementById(i)
+        .parentNode.replaceChild(newEl, document.getElementById(i));
+
+      //   el.parentNode.replaceChild(newEl, el);
+    }
+  }
 }
